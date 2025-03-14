9635616b0fdcc6bc1d5a0c2fdb1663abd414ec73
lack <valentin.dautrement@protonmail.com>
2025-03-14 15:07:59 +0100

    example

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  //Console log to show that the app is running
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
